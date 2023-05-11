ab170ac1e9fea960a789acd324833fe1a373f2af
Akbar Fadiansyah <afad0006@student.monash.edu>
2023-05-11 20:14:22 +1000

    Add athom-smart-plug module

---
+++ athom-smart-plug/src/main/kotlin/edu/monash/humanise/smartcity/athomsmartplug/AthomSmartPlugApplication.kt
@@ -0,0 +1,11 @@
+package edu.monash.humanise.smartcity.athomsmartplug
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AthomSmartPlugApplication
+
+fun main(args: Array<String>) {
+    runApplication<AthomSmartPlugApplication>(*args)
+}
---
+++ athom-smart-plug/src/test/kotlin/edu/monash/humanise/smartcity/athomsmartplug/AthomSmartPlugApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.monash.humanise.smartcity.athomsmartplug
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AthomSmartPlugApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
