3f690277fb1ba4c9b730db2958f8dfd13ce03c2d
Akbar Fadiansyah <afad0006@student.monash.edu>
2023-05-27 14:40:17 +1000

    Add logging

--- athom-smart-plug/src/main/kotlin/edu/monash/humanise/smartcity/athomsmartplug/PayloadController.kt
+++ athom-smart-plug/src/main/kotlin/edu/monash/humanise/smartcity/athomsmartplug/PayloadController.kt
@@ -1,16 +1,18 @@
 package edu.monash.humanise.smartcity.athomsmartplug
 
+import io.github.oshai.KotlinLogging
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+private val logger = KotlinLogging.logger {}
+
 @RestController
 @RequestMapping("/api/payload")
 class PayloadController {
-
     @PostMapping
     fun decodeUplinkPayload(@RequestBody payloadRequest: PayloadUplinkRequest) {
-        println("New payload: $payloadRequest")
+        logger.info { "New uplink payload to decode $payloadRequest" }
     }
 }
