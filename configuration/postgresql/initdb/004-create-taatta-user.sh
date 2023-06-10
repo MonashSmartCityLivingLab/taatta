57d83be91167c633a1487c092e1df23efe23d9c0
Akbar Fadiansyah <afad0006@student.monash.edu>
2023-06-10 20:18:49 +1000

    Add chirpstack v4 config

--- configuration/postgresql/initdb/004-create-taatta-user.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-  create role $TAATTA_POSTGRES_USER with login password '$TAATTA_POSTGRES_PASSWORD';
-EOSQL
--- configuration/postgresql/initdb/005-create-taatta-db-rhf1s001.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-  create database rhf1s001 with owner $TAATTA_POSTGRES_USER;
-EOSQL
