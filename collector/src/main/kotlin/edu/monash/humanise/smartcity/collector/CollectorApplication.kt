5ddc6a481fe3c5da2b7584f7ae3740bbe28e72dc
Akbar Fadiansyah <afad0006@student.monash.edu>
2023-06-16 17:20:24 +1000

    Auth working

--- collector/src/main/kotlin/edu/monash/humanise/smartcity/collector/CollectorApplication.kt
+++ collector/src/main/kotlin/edu/monash/humanise/smartcity/collector/CollectorApplication.kt
@@ -1,6 +1,7 @@
 package edu.monash.humanise.smartcity.collector
 
 import io.github.oshai.KotlinLogging
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.WebApplicationType
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.integration.annotation.ServiceActivator
 import org.springframework.integration.channel.DirectChannel
 import org.springframework.integration.core.MessageProducer
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory
 import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter
 import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter
 import org.springframework.messaging.MessageChannel
@@ -19,8 +21,14 @@ private val logger = KotlinLogging.logger {}
 
 @SpringBootApplication
 class CollectorApplication {
-    @Value("\${smart.city.broker.url}")
+    @Value("\${smart-city.broker-url}")
     private val brokerUrl: String? = null
+
+    @Value("\${smart-city.broker-username}")
+    private val brokerUsername: String? = null
+
+    @Value("\${smart-city.broker-password}")
+    private val brokerPassword: String? = null
 
     @Value("\${spring.application.name}")
     private val appName: String? = null
@@ -38,14 +46,31 @@ class CollectorApplication {
         val randId = UUID.randomUUID()
         val clientId = "$appName-$randId"
 
-        logger.info { "Connecting to MQTT broker at $brokerUrl with client ID $clientId" }
-        val adapter = MqttPahoMessageDrivenChannelAdapter(brokerUrl,
-                clientId,
+        val topics = arrayOf(
                 "application/+/device/+/event/+",
                 "application/+/device/+/command/+",
                 "+/sensor/+/state", // for smart plug
                 "+/status" // sensor status
         )
+
+        logger.info { "Connecting to MQTT broker at $brokerUrl with client ID $clientId" }
+        val adapter = if (brokerUsername != null && brokerPassword != null) {
+            val connectOptions = MqttConnectOptions()
+            connectOptions.userName = brokerUsername
+            connectOptions.password = brokerPassword.toCharArray()
+            val clientFactory = DefaultMqttPahoClientFactory()
+            clientFactory.connectionOptions = connectOptions
+            MqttPahoMessageDrivenChannelAdapter(brokerUrl,
+                    clientId,
+                    clientFactory,
+                    *topics
+            )
+        } else {
+            MqttPahoMessageDrivenChannelAdapter(brokerUrl,
+                    clientId,
+                    *topics
+            )
+        }
         adapter.setCompletionTimeout(5000)
         adapter.setConverter(DefaultPahoMessageConverter())
         adapter.setQos(1)
--- configuration/eclipse-mosquitto/entrypoint.sh
+++ configuration/eclipse-mosquitto/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-PASSWDFILE=/etc/mosquitto/passwd
+PASSWDFILE=/mosquitto/config/passwd
 
-if [ -z "$TAATTA_MOSQUITTO_USERNAME" ] || [ -z "$TAATTA_MOSQUITTO_PASSWORD" ]; then
+if [ -z "$TAATTA_MOSQUITTO_USER" ] || [ -z "$TAATTA_MOSQUITTO_PASSWORD" ]; then
   echo "Credentials not set, skipping"
 else
   echo "Setting credentials"
-  mosquitto_passwd -b $PASSWDFILE "$TAATTA_MOSQUITTO_USERNAME" "$TAATTA_MOSQUITTO_PASSWORD"
+  mosquitto_passwd -c -b $PASSWDFILE "$TAATTA_MOSQUITTO_USER" "$TAATTA_MOSQUITTO_PASSWORD"
 fi
 
 exec "$@"
