b6bb03f36660eb2f026fe5cde28d54365e9f301e
Akbar Fadiansyah <afad0006@student.monash.edu>
2023-06-20 23:02:34 +1000

    Make non-null entity fields to also be non-null in db

--- pcr2/src/main/kotlin/edu/monash/humanise/smartcity/pcr2/Payload.kt
+++ pcr2/src/main/kotlin/edu/monash/humanise/smartcity/pcr2/Payload.kt
@@ -1,34 +1,37 @@
 package edu.monash.humanise.smartcity.pcr2
 
 import jakarta.persistence.*
-import lombok.AllArgsConstructor
-import lombok.Builder
-import lombok.Data
-import lombok.NoArgsConstructor
 import org.hibernate.annotations.CreationTimestamp
 import org.hibernate.annotations.UpdateTimestamp
-import java.time.LocalDateTime
 import java.time.OffsetDateTime
 
 const val SEQUENCE_NAME = "pcr2_id_sequence"
 
 @Entity
 class Payload(
+    @Column(nullable = false)
     val deviceName: String,
+    @Column(nullable = false)
     val data: String,
+    @Column(nullable = false)
     val devEUI: String,
+    @Column(nullable = false)
     val ltr: Int,
+    @Column(nullable = false)
     val rtl: Int,
+    @Column(nullable = false)
     val cpuTemp: Double
 ) {
     @Id
     @SequenceGenerator(name = SEQUENCE_NAME, sequenceName = SEQUENCE_NAME)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQUENCE_NAME)
     var id: Long = 0
-    @Column(updatable = false)
+
+    @Column(nullable = false, updatable = false)
     @CreationTimestamp
     lateinit var createdAt: OffsetDateTime
 
     @UpdateTimestamp
+    @Column(nullable = false)
     lateinit var updatedAt: OffsetDateTime
 }
