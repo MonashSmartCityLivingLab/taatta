167facfc8c1f920518efdc402a76ba17ef5a1a98
Akbar Fadiansyah <afad0006@student.monash.edu>
2023-07-05 15:04:20 +1000

    athom presence sensor code skeleton

--- athom-smart-plug/src/main/kotlin/edu/monash/humanise/smartcity/athomsmartplug/PayloadUplinkRequest.kt
+++ athom-smart-plug/src/main/kotlin/edu/monash/humanise/smartcity/athomsmartplug/PayloadUplinkRequest.kt
@@ -1,19 +1,19 @@
 package edu.monash.humanise.smartcity.athomsmartplug
 
 /**
- * Payload data send from the collector.
+ * Payload data sent from the collector.
  */
 data class PayloadUplinkRequest(
-        /**
-         * Device name. This corresponds to the `esphome.name` attribute in the plug's yml file.
-         */
-        val deviceName: String = "",
-        /**
-         * Sensor name. See the module README for a list of known sensor name.
-         */
-        val sensor: String = "",
-        /**
-         * Payload data.
-         */
-        val data: String = ""
+    /**
+     * Device name. This corresponds to the `esphome.name` attribute in the plug's yml file.
+     */
+    val deviceName: String = "",
+    /**
+     * Sensor name. See the module README for a list of known sensor names.
+     */
+    val sensor: String = "",
+    /**
+     * Payload data.
+     */
+    val data: String = ""
 )
