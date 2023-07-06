c9883614029c6fe1a25b006ecb9bdfd6e5cd2a13
Akbar Fadiansyah <afad0006@student.monash.edu>
2023-07-06 20:36:57 +1000

    Change schema for df702

--- df702/src/main/java/edu/monash/humanise/smartcity/Payload.java
+++ df702/src/main/java/edu/monash/humanise/smartcity/Payload.java
@@ -1,14 +1,12 @@
 package edu.monash.humanise.smartcity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
-import jakarta.persistence.*;
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 @Data
 @Builder
@@ -16,7 +14,6 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Entity
 public class Payload {
-
     @Id
     @SequenceGenerator(
             name = "df702_id_sequence",
@@ -27,20 +24,24 @@ public class Payload {
             generator = "df702_id_sequence"
     )
     private Integer id;
+    @Column(nullable = false)
     private String deviceName;
+    @Column(nullable = false)
     private String data;
+    @Column(nullable = false)
     private String devEUI;
+    @Column(nullable = false)
     private boolean battery;
+    @Column(nullable = false)
     private boolean isFull;
+    @Column(nullable = false)
     private boolean fire;
+    @Column(nullable = false)
     private int level;
+    @Column(nullable = false)
     private int temperature;
 
-    @Column(updatable = false)
-    @CreationTimestamp
-    private LocalDateTime createdAt;
-
-    @UpdateTimestamp
-    private LocalDateTime updatedAt;
+    @Column(nullable = false)
+    private OffsetDateTime timestamp;
 
 }
--- df702/src/main/java/edu/monash/humanise/smartcity/PayloadService.java
+++ df702/src/main/java/edu/monash/humanise/smartcity/PayloadService.java
@@ -3,11 +3,16 @@ package edu.monash.humanise.smartcity;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
 @Service
 @AllArgsConstructor
 public class PayloadService {
     private final PayloadRepository payloadRepository;
     public void decodeUplinkPayload(PayloadUplinkRequest payloadRequest) {
+        var timestamp = OffsetDateTime.ofInstant(Instant.ofEpochMilli(payloadRequest.timestampMilliseconds()), ZoneOffset.UTC);
         Decoder decoder = new Decoder(payloadRequest.data());
         decoder.decode();
 
@@ -20,6 +25,7 @@ public class PayloadService {
                 .fire(decoder.isFire())
                 .level(decoder.getLevel())
                 .temperature(decoder.getTemperature())
+                .timestamp(timestamp)
                 .build();
         payloadRepository.save(payload);
     }
