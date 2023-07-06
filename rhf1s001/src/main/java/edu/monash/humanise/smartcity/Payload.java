b620b5172fa1ae5a51b6a180adfcd7493e70a4c3
Akbar Fadiansyah <afad0006@student.monash.edu>
2023-07-06 20:53:17 +1000

    Change schema for rhf1s001

--- rhf1s001/src/main/java/edu/monash/humanise/smartcity/Payload.java
+++ rhf1s001/src/main/java/edu/monash/humanise/smartcity/Payload.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 @Data
 @Builder
@@ -27,18 +28,21 @@ public class Payload {
             generator = "rhf1s001_id_sequence"
     )
     private Integer id;
+    @Column(nullable = false)
     private String deviceName;
+    @Column(nullable = false)
     private String data;
+    @Column(nullable = false)
     private String devEUI;
+    @Column(nullable = false)
     private double humidity;
+    @Column(nullable = false)
     private double temperature;
+    @Column(nullable = false)
     private int period;
+    @Column(nullable = false)
     private double batteryLevel;
 
-    @Column(updatable = false)
-    @CreationTimestamp
-    private LocalDateTime createdAt;
-
-    @UpdateTimestamp
-    private LocalDateTime updatedAt;
+    @Column(nullable = false)
+    private OffsetDateTime timestamp;
 }
