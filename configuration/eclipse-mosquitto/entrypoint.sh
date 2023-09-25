f109600f25f07b854fb86bf87e120295153d4879
Akbar Fadiansyah <afad0006@student.monash.edu>
2023-09-25 10:37:04 +1000

    Update docker compose for control ui

--- configuration/eclipse-mosquitto/entrypoint.sh
+++ configuration/eclipse-mosquitto/entrypoint.sh
@@ -7,6 +7,7 @@ if [ -z "$TAATTA_MOSQUITTO_USER" ] || [ -z "$TAATTA_MOSQUITTO_PASSWORD" ]; then
 else
   echo "$(date +%s): Setting credentials"
   mosquitto_passwd -c -b $PASSWDFILE "$TAATTA_MOSQUITTO_USER" "$TAATTA_MOSQUITTO_PASSWORD"
+  chown mosquitto:mosquitto $PASSWDFILE
 fi
 
 exec "$@"
